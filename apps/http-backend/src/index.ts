6543455418524751068518acd40a6ca243ce3c55
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-01-10 19:23:06 +0530

    chore: set up folder structure for http-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
 
+app.post("signup", (req, res) => {});
+
+app.get("signin", (req, res) => {});
+
+app.post("create-room", (req, res) => {});
+
 app.listen(3000, () => {
   console.log("express app listening on port 3000");
 });
